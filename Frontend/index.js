cbff2c389f9e51182e2f921d2da5186a72fbfaa9
Ace-h121 <chathaway121@gmail.com>
2024-03-19 07:53:12 -0400

    Added del for the fronted

--- Frontend/index.js
+++ Frontend/index.js
@@ -5,18 +5,30 @@
 async function putTeam() {
 
     let va = document.getElementById('putTeam').value
-    
-
 
     let woo = document.getElementById('putPart').value
 
-    alert(va)
 
     alert("http://127.0.0.1:8080/Allentown?Part=" + woo + "&TeamNum=" + va)
 
 
 
     const res = await fetch("http://127.0.0.1:8080/Allentown?Part=" +woo+ "&TeamNum=" +va, {
+        method: 'POST',
+        mode: "cors",
+
+    })
+    const text = await res.text()
+
+    console.log(text)
+
+  }
+
+  async function delTeam() {
+
+    let woo = document.getElementById('delPart').value
+
+    const res = await fetch("http://127.0.0.1:8080/AllentownRemove?Part=" +woo, {
         method: 'POST',
         mode: "cors",
 

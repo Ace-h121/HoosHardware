374629e422dcbd5109ede99c55740644d44593ce
Ace-h121 <chathaway121@gmail.com>
2024-03-18 22:50:29 -0400

    very very basic frontend

--- Frontend/index.js
+++ Frontend/index.js
@@ -1,9 +1,22 @@
-logMovies()
-
-async function logMovies() {
 
 
-    const res = await fetch('http://127.0.0.1:8080/AllentownRemove?Part=neo550', {
+
+
+async function putTeam() {
+
+    let va = document.getElementById('putTeam').value
+    
+
+
+    let woo = document.getElementById('putPart').value
+
+    alert(va)
+
+    alert("http://127.0.0.1:8080/Allentown?Part=" + woo + "&TeamNum=" + va)
+
+
+
+    const res = await fetch("http://127.0.0.1:8080/Allentown?Part=" +woo+ "&TeamNum=" +va, {
         method: 'POST',
         mode: "cors",
 

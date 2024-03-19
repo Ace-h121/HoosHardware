c0fc15b02732b692b29ff85435c1e1d2362fb204
Ace-h121 <chathaway121@gmail.com>
2024-03-19 09:27:36 -0400

    events selection working

--- Frontend/index.js
+++ Frontend/index.js
@@ -1,5 +1,5 @@
 
-
+getParts()
 
 
 async function putTeam() {
@@ -8,33 +8,54 @@ async function putTeam() {
 
     let woo = document.getElementById('putPart').value
 
+    let event = await getEvent()
+    alert("http://127.0.0.1:8080/"+ "Bensalem" + "?Part=" +woo+ "&TeamNum=" +va)
+    if (!(woo == "")){
+        const res = await fetch("http://127.0.0.1:8080/"+ event +"?Part=" +woo+ "&TeamNum=" +va, {
+            method: 'POST',
+            mode: "cors",
 
-    alert("http://127.0.0.1:8080/Allentown?Part=" + woo + "&TeamNum=" + va)
-
-
-
-    const res = await fetch("http://127.0.0.1:8080/Allentown?Part=" +woo+ "&TeamNum=" +va, {
-        method: 'POST',
-        mode: "cors",
-
-    })
-    const text = await res.text()
+        })
+        const text = await res.text()
 
     console.log(text)
+    }
 
-  }
+}
 
-  async function delTeam() {
+async function delTeam() {
 
     let woo = document.getElementById('delPart').value
 
-    const res = await fetch("http://127.0.0.1:8080/AllentownRemove?Part=" +woo, {
-        method: 'POST',
-        mode: "cors",
 
-    })
-    const text = await res.text()
+    let event = await getEvent()
+    if (!(woo == "")){
+        const res = await fetch("http://127.0.0.1:8080/"+ event +"Remove?Part=" +woo, {
+            method: 'POST',
+            mode: "cors",
+
+        })
+        const text = await res.text()
 
     console.log(text)
+    }
+}
+async function getEvent(){
+    
+    let team = document.getElementById('Events').value;
+    alert(team)
+    return team
+}
 
-  }
+async function getParts(){
+    let event = await getEvent()
+
+    const res = await fetch("http://127.0.0.1:8080/" + event, {
+            method: 'GET',
+            mode: "cors",
+
+        })
+        const text = await res.text()
+        console.log(text)
+        document.getElementById("list").value = text
+}
